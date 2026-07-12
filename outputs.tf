ef128404e4d1f1c43dc58fc657e829b6f442e8ee
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:03 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,41 +1,37 @@
-output "automation_dsc_configurations" {
-  description = "All automation_dsc_configuration resources"
-  value       = azurerm_automation_dsc_configuration.automation_dsc_configurations
-}
 output "automation_dsc_configurations_automation_account_name" {
-  description = "List of automation_account_name values across all automation_dsc_configurations"
-  value       = [for k, v in azurerm_automation_dsc_configuration.automation_dsc_configurations : v.automation_account_name]
+  description = "Map of automation_account_name values across all automation_dsc_configurations, keyed the same as var.automation_dsc_configurations"
+  value       = { for k, v in azurerm_automation_dsc_configuration.automation_dsc_configurations : k => v.automation_account_name }
 }
 output "automation_dsc_configurations_content_embedded" {
-  description = "List of content_embedded values across all automation_dsc_configurations"
-  value       = [for k, v in azurerm_automation_dsc_configuration.automation_dsc_configurations : v.content_embedded]
+  description = "Map of content_embedded values across all automation_dsc_configurations, keyed the same as var.automation_dsc_configurations"
+  value       = { for k, v in azurerm_automation_dsc_configuration.automation_dsc_configurations : k => v.content_embedded }
 }
 output "automation_dsc_configurations_description" {
-  description = "List of description values across all automation_dsc_configurations"
-  value       = [for k, v in azurerm_automation_dsc_configuration.automation_dsc_configurations : v.description]
+  description = "Map of description values across all automation_dsc_configurations, keyed the same as var.automation_dsc_configurations"
+  value       = { for k, v in azurerm_automation_dsc_configuration.automation_dsc_configurations : k => v.description }
 }
 output "automation_dsc_configurations_location" {
-  description = "List of location values across all automation_dsc_configurations"
-  value       = [for k, v in azurerm_automation_dsc_configuration.automation_dsc_configurations : v.location]
+  description = "Map of location values across all automation_dsc_configurations, keyed the same as var.automation_dsc_configurations"
+  value       = { for k, v in azurerm_automation_dsc_configuration.automation_dsc_configurations : k => v.location }
 }
 output "automation_dsc_configurations_log_verbose" {
-  description = "List of log_verbose values across all automation_dsc_configurations"
-  value       = [for k, v in azurerm_automation_dsc_configuration.automation_dsc_configurations : v.log_verbose]
+  description = "Map of log_verbose values across all automation_dsc_configurations, keyed the same as var.automation_dsc_configurations"
+  value       = { for k, v in azurerm_automation_dsc_configuration.automation_dsc_configurations : k => v.log_verbose }
 }
 output "automation_dsc_configurations_name" {
-  description = "List of name values across all automation_dsc_configurations"
-  value       = [for k, v in azurerm_automation_dsc_configuration.automation_dsc_configurations : v.name]
+  description = "Map of name values across all automation_dsc_configurations, keyed the same as var.automation_dsc_configurations"
+  value       = { for k, v in azurerm_automation_dsc_configuration.automation_dsc_configurations : k => v.name }
 }
 output "automation_dsc_configurations_resource_group_name" {
-  description = "List of resource_group_name values across all automation_dsc_configurations"
-  value       = [for k, v in azurerm_automation_dsc_configuration.automation_dsc_configurations : v.resource_group_name]
+  description = "Map of resource_group_name values across all automation_dsc_configurations, keyed the same as var.automation_dsc_configurations"
+  value       = { for k, v in azurerm_automation_dsc_configuration.automation_dsc_configurations : k => v.resource_group_name }
 }
 output "automation_dsc_configurations_state" {
-  description = "List of state values across all automation_dsc_configurations"
-  value       = [for k, v in azurerm_automation_dsc_configuration.automation_dsc_configurations : v.state]
+  description = "Map of state values across all automation_dsc_configurations, keyed the same as var.automation_dsc_configurations"
+  value       = { for k, v in azurerm_automation_dsc_configuration.automation_dsc_configurations : k => v.state }
 }
 output "automation_dsc_configurations_tags" {
-  description = "List of tags values across all automation_dsc_configurations"
-  value       = [for k, v in azurerm_automation_dsc_configuration.automation_dsc_configurations : v.tags]
+  description = "Map of tags values across all automation_dsc_configurations, keyed the same as var.automation_dsc_configurations"
+  value       = { for k, v in azurerm_automation_dsc_configuration.automation_dsc_configurations : k => v.tags }
 }
 
